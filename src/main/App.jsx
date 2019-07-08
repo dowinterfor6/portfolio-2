3bf9c0b0f6a8c12fcbb83eb30a738ff61f12d752
dowinterfor6 <dowinterfor6@gmail.com>
2019-07-08 14:39:49 -0700

    Add config for babel resolving .jsx files

---
+++ src/main/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/css/App.css';
+
+function App() {
+  return (
+    <section className="main-content-container">
+      <h1>HAI DIS IS MAIN CONTENT</h1>
+    </section>
+  );
+}
+
+export default App;
